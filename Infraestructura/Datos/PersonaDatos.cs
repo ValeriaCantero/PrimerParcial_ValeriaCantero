7ea4d1ed3144d3b8816096c1068c328bfb28fd4d
Valeria Cantero <valeriajcantero02@gmail.com>
2023-09-20 12:40:19 -0400

    Se sube proyecto

---
+++ Infraestructura/Datos/PersonaDatos.cs
@@ -0,0 +1,90 @@
+﻿using Infraestructura.Conexiones;
+using Infraestructura.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructura.Datos
+{
+    // Tabla Persona
+    public class PersonaDatos
+    {
+        private ConexionDB conexion;
+
+        public PersonaDatos(string cadenaConexion)
+        {
+            conexion = new ConexionDB(cadenaConexion);
+        }
+
+        public void insertarPersona(PersonaModel persona)
+        {
+            var conn = conexion.GetConexion();
+            var comando = new Npgsql.NpgsqlCommand("INSERT INTO persona(idPersona, nombre, apellido, tipoDocumento, nroDocumento," +
+                                                    "direccion, celular, email, estado)" +
+                                                "VALUES(@idPersona, @nombre, @apellido, @tipoDocumento,@nroDocumento" +
+                                                "@direccion, @celular, @email, @estado)", conn);
+            comando.Parameters.AddWithValue("idPersona", persona.idPersona);
+            comando.Parameters.AddWithValue("nombre", persona.nombre);
+            comando.Parameters.AddWithValue("apellido", persona.apellido);
+            comando.Parameters.AddWithValue("tipoDocumento", persona.tipoDocumento);
+            comando.Parameters.AddWithValue("nroDocumento", persona.nroDocumento);
+            comando.Parameters.AddWithValue("direccion", persona.direccion);
+            comando.Parameters.AddWithValue("celular", persona.celular);
+            comando.Parameters.AddWithValue("email", persona.email);
+            comando.Parameters.AddWithValue("estado", persona.estado);
+              
+            comando.ExecuteNonQuery();
+        }
+
+        public PersonaModel obtenerPersonaPorId(int id)
+        {
+            var conn = conexion.GetConexion();
+            var comando = new Npgsql.NpgsqlCommand($"Select * from persona where idPersona = {id}", conn);
+            using var reader = comando.ExecuteReader();
+            if (reader.Read())
+            {
+                return new PersonaModel
+                {
+                    idPersona = reader.GetInt32("idPersona"),
+                    nombre = reader.GetString("nombre"),
+                    apellido = reader.GetString("apellido"),
+                    tipoDocumento = reader.GetString("tipoDocumento"),
+                    nroDocumento= reader.GetString("nroDocumento"),
+                    direccion = reader.GetString("direccion"),
+                    celular = reader.GetString("celular"),
+                    email = reader.GetString("email"),
+                    estado = reader.GetString("estado"),
+
+
+                };
+            }
+            return null;
+        }
+
+        public PersonaModel EliminarPersonaPorId(int id)
+        {
+            var conn = conexion.GetConexion();
+            var comando = new Npgsql.NpgsqlCommand($"DELETE FROM persona WHERE idPersona= {id}", conn);
+            using var reader = comando.ExecuteReader();
+            return null;
+        }
+        public void modificarPersona(PersonaModel persona)
+        {
+            var conn = conexion.GetConexion();
+            var comando = new Npgsql.NpgsqlCommand($"UPDATE persona SET nombre = '{persona.nombre}', " +
+                                                          $"apellido = '{persona.apellido}', " +
+                                                          $"tipoDocumento = '{persona.tipoDocumento}', " +
+                                                          $"nroDocumento = '{persona.nroDocumento}', " +
+                                                          $"direccion = '{persona.direccion}', " +
+                                                          $"celular = '{persona.celular}', " +
+                                                          $"email = '{persona.email}' ," +
+                                                          $"estado = '{persona.estado}' " +
+                                                $" WHERE idPersona = {persona.idPersona}", conn);
+
+            comando.ExecuteNonQuery();
+        }
+    }
+}
---
+++ Servicios/ContactosService/CiudaddService.cs
@@ -0,0 +1,46 @@
+﻿using Infraestructura.Datos;
+using Infraestructura.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.ContactosService
+{
+    public class CiudaddService
+    {
+        CiudaddDatos ciudaddDatos;
+
+        public CiudaddService(string cadenaConexion)
+        {
+            ciudaddDatos = new CiudaddDatos(cadenaConexion);
+        }
+
+        public void insertarCiudad(CiudaddModel ciudad)
+        {
+            validarDatos(ciudad);
+            ciudaddDatos.insertarCiudad(ciudad);
+        }
+        public CiudaddModel obtenerCiudad(int id)
+        {
+            return ciudaddDatos.obtenerCiudadPorId(id);
+        }
+
+        public void modificarCiudad(CiudaddModel ciudad)
+        {
+            validarDatos(ciudad);
+            ciudaddDatos.modificarCiudad(ciudad);
+        }
+
+        public CiudaddModel EliminarCiudad (int id)
+        {
+            return ciudaddDatos.EliminarCiudadPorId(id);
+        }
+        private void validarDatos(CiudaddModel ciudad)
+        {
+            
+        }
+
+    }
+}
